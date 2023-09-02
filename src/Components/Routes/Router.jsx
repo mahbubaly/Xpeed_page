eddc65f00ae222b98978ee5890f69f91a30907c8
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-09-02 20:05:27 +0600

    res

--- src/Components/Routes/Router.jsx
+++ src/Components/Routes/Router.jsx
@@ -5,10 +5,7 @@ import {
 import Home from "../Home/Home";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import CardDetails from "../Home/CardDetails";
-import SignIn from "../SignIn/SignIn";
 import Main from "../Main/Main";
-import LogIn from "../SignIn/LogIn";
-import ForgetPass from "../SignIn/ForgetPass";
 import ComingPage from "../ComingPage/ComingPage";
 import ResourceCardDetails from "../Home/Resources/ResourceCardDetails";
 import HtmlProducts from "../HtmlProductPage/HtmlProducts";
@@ -34,16 +31,6 @@ const router = createBrowserRouter([
 
             },
             {
-                path: '/signUp',
-                element: <SignIn />
-
-            },
-            {
-                path: '/signIn',
-                element: <LogIn />
-
-            },
-            {
                 path: "/details/:id",
                 element: <CardDetails />,
                 loader: ({ params }) => fetch(`/details/${(params.id)}`)
@@ -56,14 +43,6 @@ const router = createBrowserRouter([
                 path: "/resource/:id",
                 element: <ResourceCardDetails />,
                 loader: ({ params }) => fetch(`/details/${(params.id)}`)
-
-
-
-
-            },
-            {
-                path: "/forgetPass",
-                element: <ForgetPass />,
 
 
 
